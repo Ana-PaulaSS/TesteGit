6a32ed33c0eeb8a08b529df33fe3735109c623f6
Ana-Paula-Grap <69468226+Ana-Paula-Grap@users.noreply.github.com>
2020-09-24 17:43:07 -0300

    Criado uma classe

---
+++ Consele/Teste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consele
+{
+    class Teste
+    {
+        public int A;
+        public int B;
+        public double C;
+    }
+}
